62bb04602ba7d3c02004978e42fe70eb31d49fed
bhandkolir <bhandkoliraju@gmail.com>
2024-09-20 12:59:26 +0000

    fixed error, plain page

--- app.js
+++ app.js
@@ -4,15 +4,6 @@ var logger = require('morgan');
 var index = require('./routes/index');
 var app = express();
 
-// Node.js version 15.1.0 and above
-const http = require('http');
-const req = new http.IncomingMessage();
-return res.render('headers', { data: JSON.stringify(req) })
-
-
-console.log(req.headers); // Output: { 'Content-Type': 'application/json' }
-console.log(Object.keys(req)); // Output: []
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
